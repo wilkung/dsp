637d832037f455314e70690dff520b3bcdad6ddd
Wilson Kung <wilsonckung@gmail.com>
2015-06-20 03:26:58 -0400

    Update lists.py

--- python/lists.py
+++ python/lists.py
@@ -2,87 +2,196 @@
 # Licensed under the Apache License, Version 2.0
 
 
-def match_ends(words):
+def donuts(count):
     """
-    Given a list of strings, return the count of the number of strings
-    where the string length is 2 or more and the first and last chars
-    of the string are the same.
-
-    >>> match_ends(['aba', 'xyz', 'aa', 'x', 'bbb'])
-    3
-    >>> match_ends(['', 'x', 'xy', 'xyx', 'xx'])
-    2
-    >>> match_ends(['aaa', 'be', 'abc', 'hello'])
-    1
+    Given an int count of a number of donuts, return a string of the
+    form 'Number of donuts: <count>', where <count> is the number
+    passed in. However, if the count is 10 or more, then use the word
+    'many' instead of the actual count.
+    >>> donuts(4)
+    'Number of donuts: 4'
+    >>> donuts(9)
+    'Number of donuts: 9'
+    >>> donuts(10)
+    'Number of donuts: many'
+    >>> donuts(99)
+    'Number of donuts: many'
     """
-    raise NotImplementedError
+#    raise NotImplementedError
+    if count > 9:
+        print 'Number of donuts: many'
+    else:
+        print 'Number of donuts: %d' % (count)
 
+donuts(10)
+donuts(3)        
 
-def front_x(words):
+def both_ends(s):
     """
-    Given a list of strings, return a list with the strings in sorted
-    order, except group all the strings that begin with 'x' first.
-    e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
-         ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'].
-
-    >>> front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa'])
-    ['xaa', 'xzz', 'axx', 'bbb', 'ccc']
-    >>> front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa'])
-    ['xaa', 'xcc', 'aaa', 'bbb', 'ccc']
-    >>> front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])
-    ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
+    Given a string s, return a string made of the first 2 and the last
+    2 chars of the original string, so 'spring' yields 'spng'.
+    However, if the string length is less than 2, return instead the
+    empty string.
+    >>> both_ends('spring')
+    'spng'
+    >>> both_ends('Hello')
+    'Helo'
+    >>> both_ends('a')
+    ''
+    >>> both_ends('xyz')
+    'xyyz'
     """
-    raise NotImplementedError
+#    raise NotImplementedError
+    if len(s)<2:
+        print "''"
+    else:
+        print s[0]+s[1]+s[-2]+s[-1]
+    
+both_ends('wilson')
+both_ends('xyz')
+both_ends('x')    
 
-
-def sort_last(tuples):
+def fix_start(b):
     """
-    Given a list of non-empty tuples, return a list sorted in
-    increasing order by the last element in each tuple.
-    e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
-         [(2, 2), (1, 3), (3, 4, 5), (1, 7)].
-
-    >>> sort_last([(1, 3), (3, 2), (2, 1)])
-    [(2, 1), (3, 2), (1, 3)]
-    >>> sort_last([(2, 3), (1, 2), (3, 1)])
-    [(3, 1), (1, 2), (2, 3)]
-    >>> sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
-    [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
+    Given a string s, return a string where all occurences of its
+    first char have been changed to '*', except do not change the
+    first char itself. e.g. 'babble' yields 'ba**le' Assume that the
+    string is length 1 or more.
+    >>> fix_start('babble')
+    'ba**le'
+    >>> fix_start('aardvark')
+    'a*rdv*rk'
+    >>> fix_start('google')
+    'goo*le'
+    >>> fix_start('donut')
+    'donut'
     """
-    raise NotImplementedError
+#    raise NotImplementedError
+    replace = b[0]
+    u = b[0]    
+    for i in (1,len(b)-1):
+        if b[i] == replace:
+            u += 't'
+        else:
+            u += b[i]
+    print u
+    
+
+fix_start('donut') #this is not working properly and I dont know why
 
 
-def remove_adjacent(nums):
+def mix_up(a, b):
     """
-    Given a list of numbers, return a list where all adjacent equal
-    elements have been reduced to a single element, so [1, 2, 2, 3]
-    returns [1, 2, 3]. You may create a new list or modify the passed
-    in list.
-
-    >>> remove_adjacent([1, 2, 2, 3])
-    [1, 2, 3]
-    >>> remove_adjacent([2, 2, 3, 3, 3])
-    [2, 3]
-    >>> remove_adjacent([3, 2, 3, 3, 3])
-    [3, 2, 3]
-    >>> remove_adjacent([])
-    []
+    Given strings a and b, return a single string with a and b
+    separated by a space '<a> <b>', except swap the first 2 chars of
+    each string. Assume a and b are length 2 or more.
+    >>> mix_up('mix', 'pod')
+    'pox mid'
+    >>> mix_up('dog', 'dinner')
+    'dig donner'
+    >>> mix_up('gnash', 'sport')
+    'spash gnort'
+    >>> mix_up('pezzy', 'firm')
+    'fizzy perm'
     """
-    raise NotImplementedError
+#    raise NotImplementedError
+    print b[0:2] + a[2:] +" "+ a[0:2] + b[2:]
 
+mix_up('dog','dinner')
+mix_up('gnash', 'sport')
 
-def linear_merge(list1, list2):
+def verbing(s):
     """
-    Given two lists sorted in increasing order, create and return a
-    merged list of all the elements in sorted order. You may modify
-    the passed in lists. Ideally, the solution should work in "linear"
-    time, making a single pass of both lists.
-
-    >>> linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc'])
-    ['aa', 'bb', 'cc', 'xx', 'zz']
-    >>> linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz'])
-    ['aa', 'bb', 'cc', 'xx', 'zz']
-    >>> linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
-    ['aa', 'aa', 'aa', 'bb', 'bb']
+    Given a string, if its length is at least 3, add 'ing' to its end.
+    Unless it already ends in 'ing', in which case add 'ly' instead.
+    If the string length is less than 3, leave it unchanged. Return
+    the resulting string.
+    >>> verbing('hail')
+    'hailing'
+    >>> verbing('swiming')
+    'swimingly'
+    >>> verbing('do')
+    'do'
     """
-    raise NotImplementedError
+#    raise NotImplementedError
+    if len(s)>2:
+        if s[-3:] == 'ing':
+            print s+'ly'
+        else:
+            print s+'ing'
+    else:
+        print s
+
+verbing('hail')
+verbing('swiming')
+verbing('do')
+
+def not_bad(s):
+    """
+    Given a string, find the first appearance of the substring 'not'
+    and 'bad'. If the 'bad' follows the 'not', replace the whole
+    'not'...'bad' substring with 'good'. Return the resulting string.
+    So 'This dinner is not that bad!' yields: 'This dinner is
+    good!'
+    >>> not_bad('This movie is not so bad')
+    'This movie is good'
+    >>> not_bad('This dinner is not that bad!')
+    'This dinner is good!'
+    >>> not_bad('This tea is not hot')
+    'This tea is not hot'
+    >>> not_bad("It's bad yet not")
+    "It's bad yet not"
+    """
+#    raise NotImplementedError
+    if 'not' in s and 'bad' in s and s.index('not') < s.index('bad'):
+        nott = s.index('not')
+        bad = s.index('bad')
+        if nott < bad:
+            print s[:nott] + 'good' + s[(bad+3):]
+    else:
+        print s
+
+not_bad('This movie is not so bad')
+not_bad('This dinner is not that bad!')
+not_bad('This tea is not hot')
+not_bad("It's bad yet not")
+
+
+def front_back(a, b):
+    """
+    Consider dividing a string into two halves. If the length is even,
+    the front and back halves are the same length. If the length is
+    odd, we'll say that the extra char goes in the front half. e.g.
+    'abcde', the front half is 'abc', the back half 'de'. Given 2
+    strings, a and b, return a string of the form a-front + b-front +
+    a-back + b-back
+    >>> front_back('abcd', 'xy')
+    'abxcdy'
+    >>> front_back('abcde', 'xyz')
+    'abcxydez'
+    >>> front_back('Kitten', 'Donut')
+    'KitDontenut'
+    """
+#    raise NotImplementedError
+    if len(a)%2.0 == 1: #odd
+        end = (len(a)/2) + 1
+        afront = a[:end]
+        aback = a[end:]
+    else:
+        end = (len(a)/2)
+        afront = a[:end]
+        aback = a[end:]
+    if len(b)%2.0 == 1: #odd
+        end = (len(b)/2) + 1
+        bfront = b[:end]
+        bback = b[end:]
+    else:
+        end = (len(b)/2)
+        bfront = b[:end]
+        bback = b[end:]
+    print afront + bfront + aback + bback        
+        
+
+front_back('abcd', 'xy')
+front_back('abcde', 'xyz')
+front_back('Kitten', 'Donut')
